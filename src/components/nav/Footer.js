db4c9c9db47a72c693595cbf0cc5fc63eef2da3f
subham5230 <subhammohanty5230@gmail.com>
2021-09-25 12:31:42 +0530

    bug fix v5

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className="mt-4 p-5 d-flex justify-content-center" style={{
+        <div className="mt-4 p-3 d-flex justify-content-center" style={{
             background: "#EB3349",
             background: "-webkit-linear-gradient(to right, #F45C43, #EB3349)",
             background: "linear-gradient(to right, #F45C43, #EB3349)",
@@ -10,7 +10,7 @@ function Footer() {
             maxWidth: "100vw",
             }}>
             
-            <h4>Harbour Ecommerce &copy; 2021, All rights reserved.</h4>
+            <h4 style={{color: "white"}}>Harbour Ecommerce &copy; 2021, All rights reserved.</h4>
         </div>
     )
 }
